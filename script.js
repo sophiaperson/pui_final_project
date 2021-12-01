65054ebff78da7b677cebdd43f474d29e7d53306
Sophia Ho <swho@andrew.cmu.edu>
2021-11-30 22:06:36 -0500

    populated json file with general trick data

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* script.js */
 
+
+/* popover functions */
 function initializePopover() {
   $(document).ready(function(){
   $('[data-toggle="popover"]').popover();
@@ -12,15 +14,39 @@ function dismissPopover() {
   });
 }
 
+// initialize popovers and dismiss them when user clicks outside
 function handlePopover() {
   initializePopover()
   dismissPopover()
 }
 
+// capitalize first letter of string
+function capitalizeFirstLetter(str) {
+  let firstLetter = str.slice(0, 1)
+  let restStr = str.slice(1)
+  return firstLetter.toUpperCase() + restStr
+}
+
+// 
+
+
+/* onload functions */
 function onLoadHome() {
 
 }
 
 function onLoadProfile() {
   handlePopover()
+}
+
+function onLoadSearch() {
+
+}
+
+function onLoadTrickDetail() {
+
+}
+
+function onLoadBrowse() {
+
 }
